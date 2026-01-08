12684620937028e60b02e4c6f0ede0fa9e80bc91
Boiyee <odjubreel2@gmail.com>
2026-01-08 02:08:33 +0100

    updated app and MyComponent

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,5 +1,22 @@
+import { Linter } from "eslint";
 import React, {useState} from "react";
 
-function MyComponent() {}
+function MyComponent() {
+
+    const [foods, setFoods] = useState(["Apple", "Banana", "Watermelon"]);
+
+    function handleAddFood() {}
+
+    function handleRemoveFood() {}
+
+    return(
+    <div>
+        <h2>List of Food</h2>
+        <ul>
+            {foods.map((food, index) => <li></li>)}
+        </ul>
+    </div>
+    );
+}
 
 export default MyComponent
