7ccb7c98c84ec43c1aa9ef7e8cdb8a2f43c5f57f
Boiyee <odjubreel2@gmail.com>
2026-01-08 02:00:51 +0100

    updated counter

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -2,26 +2,30 @@ import React, {useState} from "react";
 
 function Counter() {
 
-    const [count, setCount] = useState(0);
+    const [car, setCar] = useState({year: 2024,
+                                    make: "Ford",
+                                    model: "Mustang"});
+    
+    function handleYearChange(event) {
 
-    const increment = () => {
-        setCount(count + 1);
+        setCar(c =>  ({...c, year: event.target.value}));
     }
+    function handleMakeChange(event) {
 
-    const decrement = () => {
-        setCount(count - 1);
+        setCar(c => ({...c, make: event.target.value}));
     }
-
-    const reset = () => {
-        setCount(0);
+    function handleModelChange(event) {
+        
+        setCar(c => ({...c, model: event.target.value}));
     }
 
     return(
-        <div className="counter-container">
-            <p className="count-display">Count: {count}</p>
-            <button className="counter-button" onClick={decrement}>Decrement</button>
-            <button className="counter-button" onClick={reset}>Reset</button>
-            <button className="counter-button" onClick={increment}>Increment</button>
+        <div className="">
+            <p>You favourite car is: {car.year} {car.make} {car.model}</p>
+
+            <input type="number" value={car.year} onChange={handleYearChange}/><br />
+            <input type="text" value={car.make} onChange={handleMakeChange}/><br />
+            <input type="text" value={car.model} onChange={handleModelChange}/><br />
         </div>
     )
 }
--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,31 +1,5 @@
 import React, {useState} from "react";
 
-function MyComponent() {
-
-    const [count, setCount] = useState(0);
-
-    function increment() {
-        setCount(count + 1);
-    }
-
-    function reset() {
-        setCount(0);
-    }
-
-    function decrement() {
-        setCount(count - 1);
-    }
-    
-    return(
-        <>
-        <div>
-            <p>Count: {count}</p>
-            <button onClick={decrement}>Decrement</button>
-            <button onClick={reset}>Reset</button>
-            <button onClick={increment}>Increment</button>
-        </div>
-        </>
-    )
-}
+function MyComponent() {}
 
 export default MyComponent
