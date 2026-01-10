54b54376c1cb9ab45a36d5c2d97dc8de54d8c839
Jubreel <odjubreel2@gmail.com>
2026-01-10 14:26:19 +0100

    Use UserContext to access user in ComponentD

--- src/ComponentD.jsx
+++ src/ComponentD.jsx
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
+import React, {useContext} from "react";
+import { UserContext } from "./ComponentA";
 
 function ComponentD() {
+
+    const user = useContext(UserContext);
 
     return(
         <div className="box">
             <h1>ComponentD</h1>
-            <h2>{`Bye ${prop.user}`}</h2>
+            <h2>{`Bye ${user}`}</h2>
         </div>
     );
 }
