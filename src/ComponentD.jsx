a1b922902b52b3d58ed364866fd1775e87e301f3
Jubreel <odjubreel2@gmail.com>
2026-01-10 04:55:02 +0100

    new event

---
+++ src/ComponentD.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+function ComponentD() {
+
+    return(
+        <div className="box">
+            <h1>ComponentD</h1>
+            <h2>{`Bye ${prop.user}`}</h2>
+        </div>
+    );
+}
+
+export default ComponentD
