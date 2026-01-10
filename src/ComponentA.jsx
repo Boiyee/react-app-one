be32c37a2fdcbefeaa316b4ef39a94b384fa4cb2
Jubreel <odjubreel2@gmail.com>
2026-01-10 14:22:24 +0100

    using createContext React hook

--- src/ComponentA.jsx
+++ src/ComponentA.jsx
@@ -1,14 +1,19 @@
-import React, { useState } from 'react';
-import ComponentB from './ComponentB.jsx';
+import React, { useState, createContext } from "react";
+import ComponentB from "./ComponentB.jsx";
+
+export const UserContext = createContext();
 
 function ComponentA() {
 
-  const [user, setUser] = useState("Boiyee");
+    const [user, setUser] = useState("Boiyee");
 
     return(
         <div className="box">
             <h1>ComponentA</h1>
-            <h2>{`Hello ${Boiyee}`}</h2>
+            <h2>{`Hello ${user}`}</h2>
+            <UserContext.Provider value={user}>
+                <ComponentB user={user}/>
+            </UserContext.Provider>
         </div>
     );
 }
