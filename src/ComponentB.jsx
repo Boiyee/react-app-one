21626f89fb17b18d450f5735458ea64e107a563a
Jubreel <odjubreel2@gmail.com>
2026-01-10 04:48:40 +0100

    using new event
    Removed the user state declaration from ComponentB.

--- src/ComponentB.jsx
+++ src/ComponentB.jsx
@@ -3,8 +3,6 @@ import ComponentC from './ComponentC.jsx';
 
 function ComponentB() {
 
-  const [user, setUser] = useState("Boiyee");
-
     return(
         <div className="box">
             <h1>ComponentB</h1>
