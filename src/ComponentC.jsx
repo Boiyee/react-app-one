766eb0c8cdaed045bcb016fb23a0fdc052f02ed1
Jubreel <odjubreel2@gmail.com>
2026-01-10 14:25:00 +0100

    Refactor ComponentC to use UserContext
    Use UserContext to display user's name in ComponentC.

--- src/ComponentC.jsx
+++ src/ComponentC.jsx
@@ -1,12 +1,16 @@
-import React, { useState } from 'react';
-import ComponentD from './ComponentD.jsx';
+import React, {useContext} from "react";
+import { UserContext } from "./ComponentA";
+import ComponentD from "./ComponentD.jsx";
 
 function ComponentC() {
+
+    const user = useContext(UserContext);
 
     return(
         <div className="box">
             <h1>ComponentC</h1>
-            <ComponentD user={props.user} />
+            <h2>{`Hello ${user}`}</h2>
+            <ComponentD />
         </div>
     );
 }
